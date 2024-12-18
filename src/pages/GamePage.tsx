e3154aa2167c1e44d3f1be127d07c783736c3906
Ertuğ Akman <ertug3470@gmail.com>
2024-12-18 22:28:06 +0000

    Add number guessing functionality to GamePage

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,15 +1,46 @@
 import { RootState } from "@reduxjs/toolkit/query";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 const GamePage = () => {
+  const [userInput, setUserInput] = useState<number>(0);
+  const [messageValue, setMessageValue] = useState<string>("");
+  const [randomValue, setRandomValue] = useState<number>(0);
+
   const { fromValue, toValue } = useSelector(
     (state: RootState) => state.values
   );
 
+  const handleNumberCheck = () => {
+    if (randomValue == userInput) {
+      setMessageValue("You won!");
+    } else if (randomValue > userInput) {
+      setMessageValue("Try higher");
+    } else {
+      setMessageValue("Try lower");
+    }
+  };
+
+  useEffect(() => {
+    const randomValue =
+      Math.floor(Math.random() * (toValue - fromValue + 1)) + fromValue;
+
+    setRandomValue(randomValue);
+  }, []);
+
   return (
     <div>
-      from value : {fromValue}
-      <br /> to value : {toValue}
+      <input
+        type="number"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setUserInput(e.target.value)
+        }
+        value={userInput}
+      />
+      <button onClick={handleNumberCheck} className="bg-black">
+        Check
+      </button>
+      <p>{messageValue}</p>
     </div>
   );
 };
